58313a356e76eb4ed17f07ab89e9b219009dd4f9
eduardasartorelli <eduarda.sartorelli123@gmail.com>
2026-03-10 12:26:39 -0300

    programando botões de numero

--- calculadora/App.js
+++ calculadora/App.js
@@ -1,92 +1,115 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+ const [display, setDisplay] = useState('0');
+ const [previousValue, setPreviousValue] = (null);
+ const [operator, setOperator] = useState(null);
+ const [waitingForNewValue, setWaitingForNewValue] = useState(null);
+
+ const handleNumberPress = (num) => {
+  if (waitingForNewValue) {
+    setDisplay(String(num))
+    setWaitingForNewValue(false);
+  } else {
+    setDisplay(display === '0' ? String(num) : display + num);
+  }
+ }
+
+ const handleClear = () => {
+  setDisplay('0');
+  setPreviousValue(null);
+  setOperator(null);
+  setWaitingForNewValue(false);
+ }
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
 
       <View style={styles.displayContainer}>
           <Text style={styles.displayText} numberOfLines={1}>
-              1000
+              {display}
           </Text>
       </View>
 
+      {/* Linha 1 */}
       <View style={styles.row}>
-     <TouchableOpacity style ={styles.functionButton}>
-      <Text style={styles.functionText}>C</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style ={styles.functionButton}>
-      <Text style={styles.functionText}>+/-</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style ={styles.functionButton}>
-      <Text style={styles.functionText}>%</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style ={styles.operadorButton}>
-      <Text style={styles.operadorText}>/</Text>
-     </TouchableOpacity>
+        <TouchableOpacity style={styles.functionButton}>
+          <Text style={styles.functionText}>C</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.functionButton}>
+          <Text style={styles.functionText}>+/-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.functionButton}>
+          <Text style={styles.functionText}>%</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.operatorText}>÷</Text>
+        </TouchableOpacity>
       </View>
 
-            <View style={styles.row}>
-     <TouchableOpacity style ={styles.buttonNumber}>
-      <Text style={styles.buttonText}>7</Text>
-     </TouchableOpacity>
-       <TouchableOpacity style ={styles.buttonNumber}>
-      <Text style={styles.buttonText}>8</Text>
-     </TouchableOpacity>
-       <TouchableOpacity style ={styles.buttonNumber}>
-      <Text style={styles.buttonText}>9</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style ={styles.operadorButton}>
-      <Text style={styles.operadorText}>x</Text>
-     </TouchableOpacity>
-      </View>
-
+      {/* Linha 2 */}
       <View style={styles.row}>
-     <TouchableOpacity style ={styles.buttonNumber}>
-      <Text style={styles.buttonText}>4</Text>
-     </TouchableOpacity>
-       <TouchableOpacity style ={styles.buttonNumber}>
-      <Text style={styles.buttonText}>5</Text>
-     </TouchableOpacity>
-       <TouchableOpacity style ={styles.buttonNumber}>
-      <Text style={styles.buttonText}>6</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style ={styles.operadorButton}>
-      <Text style={styles.operadorText}>-</Text>
-     </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(7)}>
+          <Text style={styles.buttonText}>7</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(8)}>
+          <Text style={styles.buttonText}>8</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(9)}>
+          <Text style={styles.buttonText}>9</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.operatorText}>x</Text>
+        </TouchableOpacity>
       </View>
 
+      {/* Linha 3 */}
       <View style={styles.row}>
-     <TouchableOpacity style ={styles.buttonNumber}>
-      <Text style={styles.buttonText}>1</Text>
-     </TouchableOpacity>
-       <TouchableOpacity style ={styles.buttonNumber}>
-      <Text style={styles.buttonText}>2</Text>
-     </TouchableOpacity>
-       <TouchableOpacity style ={styles.buttonNumber}>
-      <Text style={styles.buttonText}>3</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style ={styles.operadorButton}>
-      <Text style={styles.operadorText}>+</Text>
-     </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(4)}>
+          <Text style={styles.buttonText}>4</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(5)}>
+          <Text style={styles.buttonText}>5</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(6)}>
+          <Text style={styles.buttonText}>6</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton} >
+          <Text style={styles.operatorText}>-</Text>
+        </TouchableOpacity>
       </View>
 
+      {/* Linha 4 */}
       <View style={styles.row}>
-     <TouchableOpacity style ={[styles.buttonNumber, styles.doubleWidthButton]}>
-      <Text style={styles.buttonText}>0</Text>
-     </TouchableOpacity>
-       <TouchableOpacity style ={styles.buttonNumber}>
-      <Text style={styles.buttonText}>.</Text>
-     </TouchableOpacity>
-     <TouchableOpacity style ={styles.operadorButton}>
-      <Text style={styles.operadorText}>=</Text>
-     </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(1)}>
+          <Text style={styles.buttonText}>1</Text >
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(2)}>
+          <Text style={styles.buttonText}>2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(3)}>
+          <Text style={styles.buttonText}>3</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.operatorText}>+</Text>
+        </TouchableOpacity>
       </View>
 
-    
-
-      
+      {/* Linha 5 */}
+      <View style={styles.row}>
+        <TouchableOpacity style={[styles.buttonNumber, styles.doubleWidthButton]}>
+          <Text style={styles.buttonText}>0</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber}>
+          <Text style={styles.buttonText}>.</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.operatorText}>=</Text>
+        </TouchableOpacity>
+      </View>
 
     </View>
   );
@@ -115,50 +138,44 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingHorizontal: 10
   },
-
-
   functionButton: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   marginHorizontal: 5,
-   borderRadius: 50,
-   height: '75%',
-   backgroundColor: '#A5A5A5',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+    borderRadius: 50,
+    height: '75%',
+    backgroundColor: '#A5A5A5',
   },
   functionText: {
-    color:'#000',
+    color: '#000',
     fontSize: 28
   },
-
-
-   operadorButton: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   marginHorizontal: 5,
-   borderRadius: 50,
-   height: '75%',
-   backgroundColor: '#ff9500',
+  operatorButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+    borderRadius: 50,
+    height: '75%',
+    backgroundColor: '#FF9500',
   },
-  operadorText: {
-    color:'#fff',
-    fontSize: 28
-  }, 
-
-
-   buttonNumber: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   marginHorizontal: 5,
-   borderRadius: 50,
-   height: '75%',
-   backgroundColor: '#3333',
+  operatorText: {
+    color: '#fff',
+    fontSize: 36
+  },
+  buttonNumber: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+    borderRadius: 50,
+    height: '75%',
+    backgroundColor: '#333333',
   },
   buttonText: {
-    color:'#fff',
-    fontSize: 28
+    color: '#fff',
+    fontSize: 32
   },
   doubleWidthButton : {
     flex: 2,
